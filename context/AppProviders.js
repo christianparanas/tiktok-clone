9d97c6e8d2370ce344cb17f3085de5805d10c962
kate <christiannparanas@gmail.com>
2022-04-26 20:48:58 +0800

    feat: userContext - auth

--- context/AppProviders.js
+++ context/AppProviders.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "./userContext";
+
 
 export default function AppProviders({ children }) {
   return (
     <React.Fragment>
-      { children }
-      <Toaster />
+      <UserProvider>
+        {children}
+        <Toaster />
+      </UserProvider>
     </React.Fragment>
-  )
+  );
 }
--- context/userContext.js
+++ context/userContext.js
@@ -1,5 +1,36 @@
+import Loader from "components/Loader";
 import db, { auth } from "lib/firebase";
-import { createContext, useState, useEffect, useContext } from "react";
+import { useRouter } from 'next/router'
+import { createContext, useEffect, useContext } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const router = useRouter()
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if(!loading && user == null) router.push('/auth')
+    
+    router.push('/')
+  }, [loading]);
+
+  if(loading) return <Loader />
+
+  return (
+    <UserContext.Provider value={[user, loading, error]}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default function useAuthUser() {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error("useAuthUser must be used with UserContext.Provider");
+  }
+
+  return context;
+}
